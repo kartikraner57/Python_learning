a626e01acaed223df1d1fd998fd4d83352bfd91f
kartikraner57 <kartikraner06gmail.com>
2024-12-16 15:34:00 +0530

    add dictionary program

--- lecture_four.py
+++ lecture_four.py
@@ -1,1 +1,28 @@
-print("lecture four")
+# print("lecture four")
+
+# ###dictionary###
+# info = {
+#     "key" : "value",
+#     "name" : "apnacollage",
+#     "age" : "35",
+#     "is_adult" : "True",
+#     "12.99" : "94.4"
+# }
+# null_dict  = {}
+# print(null_dict)
+# print(info)
+# info["name"]= "kartik raner"
+# print(info)
+
+##nested dictionaries
+student ={
+    "name":"kartik",
+    "score":{
+        "chem": 98,
+        "phy": 97,
+        "math" : "95"
+    }}
+print(student)
+print(student["score"]["math"])
+print(student["score"]["phy"])
+print(student["score"]["chem"])
