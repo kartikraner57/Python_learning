e7df112231362e782a19d85b6012e0b02646da61
kartikraner57 <kartikraner06gmail.com>
2024-12-16 16:11:19 +0530

    add methods of dictionary

--- lecture_four.py
+++ lecture_four.py
@@ -15,19 +15,33 @@
 # print(info)
 
 # ##nested dictionaries
-student ={
-    "name":"kartik",
-    "score":{
-        "chem": 98,
-        "phy": 97,
-        "math" : "95"
-    }}
-print(student)
-print(student["score"]["math"])
-print(student["score"]["phy"])
-print(student["score"]["chem"])
-print(list(student.values()))
+# student ={
+#     "name":"kartik",
+#     "score":{
+#         "chem": 98,
+#         "phy": 97,
+#         "math" : "95"
+#     }}
+# print(student)
+# print(student["score"]["math"])
+# print(student["score"]["phy"])
+# print(student["score"]["chem"])
+# print(list(student.values()))
+
+
 ##Dictionary methods
-#1).keys() #Returns a view object that displays all the keys in the dictionary.
-my_dict = {"a": 1, "b": 2}
-print(my_dict.keys())  # Output: dict_keys(['a', 'b'])
+# #1).keys() #Returns a view object that displays all the keys in the dictionary.
+# my_dict = {"a": 1, "b": 2}
+# print(my_dict.keys())  # Output: dict_keys(['a', 'b'])
+
+# #2.values() #Returns a view object that displays all the values in the dictionary.
+# my_dict = {"a": 1, "b": 2}
+# print(my_dict.values())  # Output: dict_values([1, 2])
+
+# #3).items() #This method is commonly used to iterate over both keys and values in a dictionary.
+# my_dict = {"a": 1, "b": 2}
+# for key, value in my_dict.items():
+#     print(key, value)
+# # Output:
+# # a 1
+# # b 2
