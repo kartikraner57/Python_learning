b89d8f99ef171977f9c0cf742e6f26d4e36217fd
kartikraner57 <kartikraner06gmail.com>
2024-12-20 15:06:29 +0530

    add while loop

--- lecture_five.py
+++ lecture_five.py
@@ -1,2 +1,31 @@
-print("hello")
-print("dictionary")
+####loops #loops are used to repeat instructions.
+
+# #while loop
+# while True :
+#     print("hello")
+
+# count = 1
+# while count <=5 :
+#     print("hello")
+#     count += 1
+#     print(count)
+
+
+
+# i = 1
+# while i <= 5:
+#     print("hello"
+#     i+=1
+
+# #print numbers from 1 to 5
+# i = 5
+# while i >= 5:
+#     print(i)
+#     i -= 1
+# print("loop ended")
+
+#print number from 1 to 100.
+i = 1
+while i <= 100:
+    print(i)
+    i += 1
--- lecture_two.py
+++ lecture_two.py
@@ -142,4 +142,3 @@ else:
 
     # """simple code"""
 
-print("hello")
