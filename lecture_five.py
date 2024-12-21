e0d3e7fdd6b834b70eb0a270d260de06d75a09fa
kartikraner57 <kartikraner06gmail.com>
2024-12-22 00:32:23 +0530

    add for loop example

--- lecture_five.py
+++ lecture_five.py
@@ -43,16 +43,29 @@
 #     print(n*i)
 #     i += 1
 
-#print the element of the following list using a loop
-# [1,4,9,16,25,36,49,64,81,100]
-nums = [1,4,9,16,25,36,49,64,81,100]
-idx = 0
-while idx < len(nums):
-    print(nums[idx]) #num[0],nums[2],num[2]..
-    idx += 1
+# #print the element of the following list using a loop
+# # [1,4,9,16,25,36,49,64,81,100]
+# nums = [1,4,9,16,25,36,49,64,81,100]
+# idx = 0
+# while idx < len(nums):
+#     print(nums[idx]) #num[0],nums[2],num[2]..
+#     idx += 1
 
-heroes = ["ironman","thor","superman","batman"]
-idx = 0
-while idx < len(heroes):
-    print(heroes[idx])
-    idx += 1
+# heroes = ["ironman","thor","superman","batman"]
+# idx = 0
+# while idx < len(heroes):
+#     print(heroes[idx])
+#     idx += 1
+# for i in range(5):
+#     print(i)
+
+#     fruits = ["apple", "banana", "cherry"]
+# for fruit in fruits:
+#     print(fruit)
+
+# for i in range(0, 10, 2):
+#     print(i)
+
+# for i in range(3):
+#     for j in range(2):
+#         print(f"i: {i}, j: {j}")
