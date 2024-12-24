0f0c5e90de7fc692c228da697ca607c8c47d677b
kartikraner57 <kartikraner06gmail.com>
2024-12-24 12:37:30 +0530

    add for loop

--- lecture_five.py
+++ lecture_five.py
@@ -69,3 +69,96 @@
 # for i in range(3):
 #     for j in range(2):
 #         print(f"i: {i}, j: {j}")
+
+# nums = (1,4,9,16,25,36,49,64,81,100)
+
+# x =36
+
+# i = 0 #
+# while i < len(nums):
+#     print(nums[i])
+#     i += 1
+
+
+# nums = (1,4,9,16,25,36,49,64,81,100)
+
+# x =36
+
+# i = 0 #
+# while i < len(nums):
+#     if(nums[i] == x):
+#         print("found at inx", i)
+
+#     i += 1
+
+
+# nums = (1,4,9,16,25,36,49,64,81,100,36)
+
+# x =36
+
+# i = 0 #
+# while i < len(nums):
+#     if(nums[i] == x):
+#         print("found at inx", i)
+#     else:
+#         print("finding ..")
+
+#     i += 1
+
+
+
+  ##  break and continue
+  #break : use to termine the loop when encountered
+  #couninue : terminates exicution in the current iter
+
+# i = 1 
+# while i <= 5:
+#     print(i)
+#     if(i== 3):
+#         break
+#     i += 1
+
+# print("end of loop")
+
+
+# nums = (1,4,9,16,25,36,49,64,81,100,)
+
+# x =36
+
+# i = 0 #
+# while i < len(nums):
+#     if(nums[i] == x):
+#         print("found at inx", i)
+#         break
+#     else:
+#         print("finding ..")
+#     i += 1
+
+# print("end of loop")
+
+
+
+# i = 0
+# while i <= 5:
+#     if(i == 3) :
+#         i+= 1
+#         continue #skip
+#     print(i)
+#     i += 1
+
+# ####for loop ## this loop used to for sequential traverse. for traversing list,string,tuples etc.
+# veggies = ["potato","brijal", "ladyfinger","cucumber"]
+
+# for val in veggies :
+#     print(val)
+# tup =(1,2,3,4,5,)
+# for num in tup :
+#     print(num)
+
+str = "apnacollage"
+for char in str:
+    if(char == 'o'):
+     print("o found")
+     break
+    print(char)
+print("END")
