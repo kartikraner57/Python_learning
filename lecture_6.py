ba48dc8cac4f10c09d9f511e8654338f0b5f24c1
kartikraner57 <kartikraner06gmail.com>
2024-12-25 08:22:02 +0530

    add function

---
+++ lecture_6.py
@@ -0,0 +1,72 @@
+# ###function = bloack of statement that perform a specific task
+# def calc_sum(a,b):
+#     sum = a + b
+#     print(sum)
+#     return sum
+
+# calc_sum(5,2)
+# calc_sum(2,10)
+# calc_sum(55,43)
+
+
+
+# a = 5
+# b = 2
+
+# sum = a+b
+# print(sum)
+
+# # kore lines code
+# a = 2
+# b = 10
+# sum = a + b
+# print(sum)
+
+# # more lines code
+# a = 55
+# b = 43
+# sum = a+ b
+# print(sum)
+
+# #function defination
+# def calc_sum(a,b):# perameters
+#     return a + b
+# sum = calc_sum(1,3) #function call; arguments
+# # calc_sum(5,6)
+# print(sum)
+
+# def calc_sum(a,b):# perameters
+#     return a + b
+# sum = calc_sum(1,3) #function call; arguments
+# # calc_sum(5,6)
+# print(sum)
+
+# def print_hello():
+#     print("hello")
+
+
+# print_hello()
+# print_hello()
+# print_hello()
+# print_hello()
+# print_hello()
+
+# def print_hello():
+#     print("hello")
+
+# output = print_hello()
+# print(output) #none
+
+# def calc_avg(a,b,c):
+#     sum = a + b + c
+#     avg = sum / 3
+#     print(avg)
+#     return avg
+# calc_avg(100,50,30)
+# print("apnacollage",end=" & ") #sep = " "
+# print("shradhakhapra") #end = "\n"= next line
+
+def cal_prod(a = 2,b = 5):
+    print(a * b)
+    return a * b
+cal_prod(1)
