ef81cb95d7af3e09fee58d45f89d543d4b9a0c76
kartikraner57 <kartikraner06@gmail.com>
2025-01-02 15:03:41 +0530

    add data types

---
+++ python fundamentals/python reserved words/code.py
@@ -0,0 +1,10 @@
+import keyword
+
+# Get a list of Python reserved keywords
+reserved_keywords = keyword.kwlist
+
+# Print the number of reserved keywords
+print("Number of reserved keywords:", len(reserved_keywords))
+
+# # Print all reserved keywords
+# print("Reserved keywords:", reserved_keywords)
