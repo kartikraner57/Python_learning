0f967b277795179d3340f8c321deeeedd90a8c3a
kartikraner57 <kartikraner06@gmail.com>
2025-01-04 17:30:27 +0530

    ADD

---
+++ python fundamentals/int data type/code.py
@@ -0,0 +1,17 @@
+a = 10
+print(type(a))
+
+
+
+b = 1111
+print(b)
+
+
+
+c = 0B1111
+print(c)
+# d = 0o786
+# print(d)
+print( '*' * 10 + ' Converting from float to int data types ' + '*' * 10)
+a = int(10.9898)
+print(a)
