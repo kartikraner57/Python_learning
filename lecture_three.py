0ef2d6c43d5ce5ea3ba96779b6b6790e1cc20873
kartikraner57 <kartikraner06@gmail.com>
2025-01-17 23:33:49 +0530

    add

--- lecture_three.py
+++ lecture_three.py
@@ -196,3 +196,163 @@
 # grade.sort()
 # print(grade)
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# str = "hello"
+# print(str[0])
+# #[0] = "y" # beacuse of string is immutable
+
+# marks =[85, 94, 76, 63, 48]
+# print(marks[-3:-1])
+
+
+###list function####
+# list = [2,1,3]
+# list.append(4) #taking in last
+# print(list)
+
+# list = [2,1,3]
+# list.append(4)
+# print(list.sort(reverse = True))#sort = sequence 
+# print(list.sort())
+# print(list)
+
+
+# list = [2,1,3]
+# list.reverse()
+# print(list)
+# print(list.sort())
+# print(list)
+
+
+
+# list = [2,1,3]
+# list.insert(3,8)
+# print(list)
+# list.insert(2,0)
+# print(list)
+
+
+# list = [2,5,3]
+# list.remove(2)
+# print(list)
+
+
+# list = [2,1,3]
+# list.pop(2)
+# print(list)
+
+##append
+# my_list = [1, 2, 3]
+# my_list.append(4)  # Adds 4 at the end
+# print(my_list)  # Output: [1, 2, 3, 4]
+
+##extend
+# my_list = [1, 2, 3]
+# my_list.extend([4, 5])  # Adds 4 and 5 to the list
+# print(my_list)  # Output: [1, 2, 3, 4, 5]
+
+##insert
+# my_list = [1, 2, 3]
+# my_list.insert(1, 'a')  # Inserts 'a' at index 1
+# print(my_list)  # Output: [1, 'a', 2, 3]
+
+##remove()
+# my_list = [1, 2, 3, 2]
+# my_list.remove(2)  # Removes the first 2
+# print(my_list)  # Output: [1, 3, 2]
+
+##pop()
+# my_list = [1, 2, 3]
+# popped_element = my_list.pop(1)  # Removes the element at index 1 (which is 2)
+# print(popped_element)  # Output: 2
+# print(my_list)  # Output: [1, 3]
+
+##index()
+# my_list = [1, 2, 3, 2]
+# index_of_2 = my_list.index(2)  # Finds the index of the first occurrence of 2
+# print(index_of_2)  # Output: 1
+
+##count()
+# my_list = [1, 2, 3, 2, 2]
+# count_of_2 = my_list.count(2)  # Counts how many times 2 appears in the list
+# print(count_of_2)  # Output: 3
+
+##sort()
+# my_list = [3, 1, 2]
+# my_list.sort()  # Sorts the list
+# print(my_list)  # Output: [1, 2, 3]
+
+##reverse()
+# my_list = [1, 2, 3]
+# my_list.reverse()  # Reverses the list
+# print(my_list)  # Output: [3, 2, 1]
+
+##clear()
+# my_list = [1, 2, 3]
+# my_list.clear()  # Clears the list
+# print(my_list)  # Output: []
+
+
+####tuple####
+
+# tup = (2, 1, 3, 1)
+# print(type(tup))
+# print(tup[0])
+# print(tup[2])
+#tup[0] = 5  #wrong beacuse of immutable
+
+# tup = ()
+# print(tup)
+# print(type(tup))
+
+# tup = (2, 1, 5, 1)
+# print(tup[1:3])
+
+# tup = (2,1,3,1)
+# print(tup.index(2))
+# print(tup.index(3))
+# print(tup.index(1))
+
+#tup = (2,1,3,1)
+# print(tup.count(2))
+# print(tup.count(3))
+# print(tup.count(1))
+
+
+# movies = []
