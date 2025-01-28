7ce137277ffafe42d3899fcff3fd3416a827de90
kartikraner57 <kartikraner06@gmail.com>
2025-01-28 22:47:22 +0530

    add

---
+++ python fundamentals/Base Conversion Functions/code.py
@@ -0,0 +1,10 @@
+# # bin function
+# print(bin(15))
+# print(bin(0o123))
+# print(0xFAce)
+# # octfunction
+
+print(oct(0b111101))
+print(oct(0xFace))
+print(hex(1000))
+print(hex(0o123456))
---
+++ python fundamentals/float data type/code.py
@@ -0,0 +1,7 @@
+#float
+a = 123.4
+print(type(a))
+      
+    #   exponential form =aplicable for float
+f = 12e3 #e = 10^()
+print(f)
