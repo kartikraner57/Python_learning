a88c29f88018a726a9058639cab6ff5f5eb4f9d3
kartikraner57 <kartikraner06@gmail.com>
2025-01-28 23:47:48 +0530

    add

---
+++ python fundamentals/complex data type/code.py
@@ -0,0 +1,18 @@
+# Defining a complex number
+z = 3 + 4j  # 3 is the real part, 4 is the imaginary part
+print(z)  # Output: (3+4j)
+
+z1 = 3 + 4j
+z2 = 1 + 2j
+result = z1 + z2
+print(result)  # Output: (4+6j)
+
+result = z1 - z2
+print(result)  # Output: (2+2j)
+
+result = z1 * z2
+print(result)  # Output: (-5+10j)
+
+result = z1 / z2
+print(result)  # Output: (2.2+0.4j)
+  
