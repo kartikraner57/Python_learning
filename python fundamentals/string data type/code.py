e7e9a5597ac9491e605b13f94336abc9e4d87d01
kartikraner57 <kartikraner06@gmail.com>
2025-02-07 22:18:33 +0530

    Add

--- python fundamentals/string data type/code.py
+++ python fundamentals/string data type/code.py
@@ -1,8 +1,28 @@
 # Single quotes
 my_string = 'Hello, World!'
+print(type(my_string))
 
 # Double quotes
 another_string = "Python is awesome!"
 
 print(my_string)
 print(another_string)
+
+
+single_quote_string = 'This is a string using single quotes.'
+print(single_quote_string)
+
+double_quote_string = "This is a string using double quotes."
+print(double_quote_string)
+
+triple_double_quote_string = """This is another example
+of a multi-line string."""
+print(triple_double_quote_string)
+
+
+combined_string = "This is a string with 'single quotes' inside double quotes."
+print(combined_string)
+
+triple_combined_string = '''This string contains both "double quotes" and 'single quotes'.'''
+print(triple_combined_string)
+
