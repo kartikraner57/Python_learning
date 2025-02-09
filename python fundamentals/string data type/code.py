0924e5c1bf2da17541bda7e2d95a371831f8a498
kartikraner57 <kartikraner06@gmail.com>
2025-02-09 16:25:19 +0530

    add

--- python fundamentals/string data type/code.py
+++ python fundamentals/string data type/code.py
@@ -38,4 +38,19 @@
 
 
 
-# 
+#Index of string data type
+# s = 'durga'
+# print(s[0])
+# print(s[1])
+# print(s[2])
+# #print(s[100]) this is error
+# print(s[-1])
+# print(s[-2])
+
+
+# #Multiple string letters
+# s = "class durga by 'durga' very good"
+# print(s)
+
+# s1 = 'class durga by "durga"very good'
+# print(s1)
