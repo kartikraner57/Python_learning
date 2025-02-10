75ef54e06b30566f288f8f8616fe640691dec4ba
kartikraner57 <kartikraner06@gmail.com>
2025-02-10 22:36:06 +0530

    add

---
+++ python fundamentals/string data type/opperators.py
@@ -0,0 +1,16 @@
+# # + operator
+
+# s = 'durga'+ 'soft'
+# print(s)
+# w1 ='durga'+ 10
+# print(s1) # error int + string  is allow except python language
+
+# # * oprator
+# s1= 'durga'* 3
+# print(s1)
+# s2= 3 * 'durga'
+# print(s2)
+# s3 = 'durga' * 'soft'
+# print(s3)#error
+
+print('#' * 10)
