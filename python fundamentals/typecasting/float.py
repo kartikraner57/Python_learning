9ad6e0d41a90f110197b4f27495f1a0f5ee58a8f
kartikraner57 <kartikraner06@gmail.com>
2025-02-11 23:56:44 +0530

    add

---
+++ python fundamentals/typecasting/float.py
@@ -0,0 +1,21 @@
+# #int
+# print(float(15))
+# #complex
+# print(float(10+20j))
+# #Bool
+# print(float(True))
+# print(float(False))
+#string
+print(float('10'))
+print(float(20.6))
+# print(float('ox Face'))
+# print(float('durga'))
+print(float(True))
+
+# complex
+print(complex("10+10j"))
+print(complex("10.5+0j"))
+print(complex("1+0j"))
+print(complex("10+0j"))
+print(complex("10.5+0j"))
+# print(complex(""))
