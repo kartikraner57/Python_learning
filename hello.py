2a5839313bf1fb41aa22084fbd00d3ad581c4664
kartikraner57 <kartikraner06@gmail.com>
2025-02-13 23:25:18 +0530

    add

--- hello.py
+++ hello.py
@@ -8,3 +8,82 @@ print("hello there")
 
 print("my place")
 print("hello there")
+
+
+####string####
+
+# str1 = "this is a string."
+# str2 = 'apnacollage'
+# str2 = """this is a string"""
+
+# str1 ="this is astring. \n and \t we are creating it in python."
+# print(str1)
+
+
+
+
+# a = int(input("enter first number : "))
+# b = int(input("enter second number : "))
+# c = int(input("enter third number : "))
+
+# if(a >= b and a >= c):
+#     print("first number is largest",a)
+# elif(b >= c and b >= a):
+#     print("second number is largest",b)
+# else:
+#     print("third is largest",c)
+
+
+
+
+
+
+
+
+
+# str1 = "apna"
+# str2 = "collage"
+# final_str = str1+str2
+# print(final_str) #1
+# print(str1 + str2)#2
+
+
+# str1 = "apna"
+# len1 = (len(str1))
+# print(len1)
+# str2 = "collage"
+# len2 = len(str2)
+# print(len2)
+# final_str = str1+  str2
+# print(final_str)
+# print(len(final_str))
+
+# ####indexing#####
+
+# str ="apna collage"
+# print(str[3])
+
+
+####slicing###
+# str  = "apna collage"
+# print(str[1:4])
+# print(str[1:len(str)])
+# print(str[:4])
+# print(str[1:])
+
+#####negative slicing####
+# str = "apple"
+# print(str[-5:-2])
+# print(str[-5:-1])
+
+####function#####
+
+
+# str = "i am from studying python from Apnacollage"
+# print(str.endswith("ege"))
+# str = (str.capitalize())
+# print(str)
+# print(str.replace("o","a"))
+# print(str.find("o"))
+# print(str.find("a"))
+# print(str.find("from"))
