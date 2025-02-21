65760c2a6a8c3395a5473742977db7426b965815
kartikraner57 <kartikraner06@gmail.com>
2025-02-21 23:51:18 +0530

    add

--- firstprogram.py
+++ firstprogram.py
@@ -1,153 +1,157 @@
-# print("kartik raner")
-# print("my age is 23")
-# print("23 , 25")
-#  name ="shradha"
-# age = 23
-# price = 25.99
-# # age2 = age
+# # print("kartik raner")
+# # print("my age is 23")
+# # print("23 , 25")
+# #  name ="shradha"
+# # age = 23
+# # price = 25.99
+# # # age2 = age
 
-# print(price)
+# # print(price)
 
-# print("my name is :",name)
-# print("my age is :",age)
-# print(age)
+# # print("my name is :",name)
+# # print("my age is :",age)
+# # print(age)
 
-# print(type(name))
-# print(type(age))
-# print(type(price))
-
-
-# name1 = "sk"
-# name2 ='skk'
-#print(name1,name2 )
-# age = 23
-# old = False
-# a = None 
-# print(type(old))
-# print(type(a))
-# a = 2
-# b = 5 
-# sum = a + b 
-# print(sum)
+# # print(type(name))
+# # print(type(age))
+# # print(type(price))
 
 
-
-# #print("hellow world")
-
-# #print("hellow world")
-
-
-# #arethemetic operration
-# a = 5
-# b = 2
-
-
-# print(a + b)
-# print(a- b)
-# print(a * b)
-# print(a / b )
-# print(a % b) #remainder
-# print(a ** b)#a^b
-
-# #relational operators
-# a = 50 
-# b = 20
-
-# print(a == b) #False
-# print(a != b) #True
-# print(a >= b)#True
-# print(a > b) #True
-# print(a <= b) #False
-# print(a < b)  #False
-
-# #assaiment operators
-# num = 10 
-# num = num + 10 #10+10 => 20
-# num += 10
-# print ("num :", num)
-# num = 10
-# num **= 5
-# print ("num",num) #50
-
-# #logical operators
-
-# a = 50
-# b = 30
-# print(not False)
-# print(not (a > b))
+# # name1 = "sk"
+# # name2 ='skk'
+# #print(name1,name2 )
+# # age = 23
+# # old = False
+# # a = None 
+# # print(type(old))
+# # print(type(a))
+# # a = 2
+# # b = 5 
+# # sum = a + b 
+# # print(sum)
 
 
-# val1 = False
-# val2 = True
-# print ("AND operator:", val1 and val2)
 
-# print ("OR operator :", val1 or val2)
+# # #print("hellow world")
 
-#type conversion
-# a = "2"
-# b = 4.25
-# print (type (b))
-# print (type(a))
-# sum = (a + b) # 2.5 + 4.25 => 6.25
-# print (sum)
-#
-#
- # type cast
+# # #print("hellow world")
+
+
+# # #arethemetic operration
+# # a = 5
+# # b = 2
+
+
+# # print(a + b)
+# # print(a- b)
+# # print(a * b)
+# # print(a / b )
+# # print(a % b) #remainder
+# # print(a ** b)#a^b
+
+# # #relational operators
+# # a = 50 
+# # b = 20
+
+# # print(a == b) #False
+# # print(a != b) #True
+# # print(a >= b)#True
+# # print(a > b) #True
+# # print(a <= b) #False
+# # print(a < b)  #False
+
+# # #assaiment operators
+# # num = 10 
+# # num = num + 10 #10+10 => 20
+# # num += 10
+# # print ("num :", num)
+# # num = 10
+# # num **= 5
+# # print ("num",num) #50
+
+# # #logical operators
+
+# # a = 50
+# # b = 30
+# # print(not False)
+# # print(not (a > b))
+
+
+# # val1 = False
+# # val2 = True
+# # print ("AND operator:", val1 and val2)
+
+# # print ("OR operator :", val1 or val2)
+
+# #type conversion
+# # a = "2"
+# # b = 4.25
+# # print (type (b))
+# # print (type(a))
+# # sum = (a + b) # 2.5 + 4.25 => 6.25
+# # print (sum)
+# #
+# #
+#  # type cast
 
  
-# a= int ("2")
-# b = 4.25
-# print(type(a))
-# print (a + b)
+# # a= int ("2")
+# # b = 4.25
+# # print(type(a))
+# # print (a + b)
 
-# a= int ("3")
-# b = 5.25
-# print(type(a))
-# print (a + b)
-
-
-# a = 3.14
-# a = str(a)
-# print (type (a))
-
-# ####input
-
-# name = input ("enter your age :")
-# print ("you entered ", name)
-
-# language fundamentals :
-
-# #introduction 
+# # a= int ("3")
+# # b = 5.25
+# # print(type(a))
+# # print (a + b)
 
 
-# Numeric Types
-x = 10            # int
-y = 3.14          # float
-z = 2 + 3j        # complex
+# # a = 3.14
+# # a = str(a)
+# # print (type (a))
 
-# Sequence Types
-name = "Alice"    # str
-fruits = ["apple", "banana", "cherry"]  # list
-coordinates = (10, 20)  # tuple
+# # ####input
 
-# Mapping Type
-person = {"name": "Alice", "age": 25}  # dict
+# # name = input ("enter your age :")
+# # print ("you entered ", name)
 
-# Set Types
-fruits_set = {"apple", "banana", "cherry"}  # set
-frozen_set = frozenset([1, 2, 3])  # frozenset
+# # language fundamentals :
 
-# Boolean Type
-is_active = True    # bool
+# # #introduction 
 
-# Binary Types
-b = bytes([65, 66, 67])  # bytes
-ba = bytearray([65, 66, 67])  # bytearray
 
-# Special Type
-#value = None  # NoneType
-#print(null_dict)
-# print(info)
-# info["name"]= "kartik raner"
-# print(info)
+# # Numeric Types
+# x = 10            # int
+# y = 3.14          # float
+# z = 2 + 3j        # complex
 
+# # Sequence Types
+# name = "Alice"    # str
+# fruits = ["apple", "banana", "cherry"]  # list
+# coordinates = (10, 20)  # tuple
+
+# # Mapping Type
+# person = {"name": "Alice", "age": 25}  # dict
+
+# # Set Types
+# fruits_set = {"apple", "banana", "cherry"}  # set
+# frozen_set = frozenset([1, 2, 3])  # frozenset
+
+# # Boolean Type
+# is_active = True    # bool
+
+# # Binary Types
+# b = bytes([65, 66, 67])  # bytes
+# ba = bytearray([65, 66, 67])  # bytearray
+
+# # Special Type
+# #value = None  # NoneType
+# #print(null_dict)
+# # print(info)
+# # info["name"]= "kartik raner"
+# # print(info)
+
+a = 5
+b =5
+c= 4
+print(a + c  * b)
