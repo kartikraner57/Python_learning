b6a8e086d0121eeea02eb452c7dc7e4ff019ef86
kartikraner57 <kartikraner06@gmail.com>
2025-03-01 23:17:07 +0530

    add

--- python fundamentals/typecasting/str.py
+++ python fundamentals/typecasting/str.py
@@ -5,3 +5,13 @@ print(str('10'))
 a = [1,3,4,56,7,8,]
 print(type(a))
 print(a[3])
+
+fruits = ["apple", "banana", "cherry"]
+
+fruits.append("orange")
+
+fruits.remove("banana")
+
+
+for fruit in fruits:
+    print(fruit)
