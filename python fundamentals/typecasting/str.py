fd45619fdf6dd376b8fbaa2419aedc0c0add7cc0
kartikraner57 <kartikraner06@gmail.com>
2025-03-08 23:48:56 +0530

    add

--- python fundamentals/typecasting/str.py
+++ python fundamentals/typecasting/str.py
@@ -1,19 +1,21 @@
-print(str('10'))
+# print(str('10'))
 
 
 
-a = [1,3,4,56,7,8,]
-print(type(a))
-print(a[3])
+# a = [1,3,4,56,7,8,]
+# print(type(a))
+# print(a[3])
 
 
 
-fruits = ["apple", "banana", "cherry"]
-fruits.append("orange")
-fruits.remove("banana")
-for fruit in fruits:
-    print(fruit)
-print(len(fruits))
-print("add")
+# fruits = ["apple", "banana", "cherry"]
+# fruits.append("orange")
+# fruits.remove("banana")
+# for fruit in fruits:
+#     print(fruit)
+# print(len(fruits))
+# print("add")
 
-git
+a = 10
+b = 20
+print((a+b)**5)
