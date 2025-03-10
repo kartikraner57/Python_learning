8768a03eeb8226149a296a498af0bcaf57a56ea6
kartikraner57 <kartikraner06@gmail.com>
2025-03-10 06:08:37 +0530

    add

--- python fundamentals/typecasting/int.py
+++ python fundamentals/typecasting/int.py
@@ -1,14 +1,14 @@
 #float
 print(int(10.989)) #output=10
-# #complex
+# # #complex
 # print(int(10+20j)) #output= type error
 #bool
 print(int(True))
 print(int(False))
 #string
 print(int('15'))
-#print(int('0B1111')) #type error
-#print(int('15.5')) #type error
+# print(int('0B1111')) #type error
+# print(int('15.5')) #type error
 
 
 
