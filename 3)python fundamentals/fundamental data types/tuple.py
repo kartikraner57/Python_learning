7a13b5359ce785e5c855f285c0bb2ea407bd013f
kartikraner57 <kartikraner06@gmail.com>
2025-03-10 23:05:07 +0530

    add

---
+++ 3)python fundamentals/fundamental data types/tuple.py
@@ -0,0 +1,13 @@
+#tuple this is immutable, (), less memory, faster access(performence is more)
+k =(10,20,30,40,50,60,"durga")
+print(k)
+print(type(k))
+print(k[1])
+print(k[-3])
+print(k[0:-4])
+#appen(),removw() are not applicable for tuple because immutable
+k = ()
+k =(10)
+print(type(k))
+k = (10,)
+print(type(k))
