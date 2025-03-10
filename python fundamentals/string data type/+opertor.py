f30680e5381370afc96e7d710dd9f92cf8f0e6ff
kartikraner57 <kartikraner06@gmail.com>
2025-03-10 05:56:30 +0530

    add

---
+++ python fundamentals/string data type/+opertor.py
@@ -0,0 +1,13 @@
+# s = "durga"+"soft"
+# print(s)#=>softdurga
+# s1 = "durga"*3
+# print(s1)#=>durga durga durga
+# print("#0"*10)
+# print("durgasoft")
+# print("#"*10)
+
+# # sum error
+# a = 10 + "surga"
+# print(a) #=>error :-this applicable in java not in python
+w = ("durga"*"soft")
+print(w)
