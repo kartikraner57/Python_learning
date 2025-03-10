a254053676a6c2d11ae975d56704cd415b9bf726
kartikraner57 <kartikraner06@gmail.com>
2025-03-10 22:37:44 +0530

    add

---
+++ 3)python fundamentals/fundamental data types/list.py
@@ -0,0 +1,22 @@
+# l = [10,39,"add"]
+# print(l)
+# print(type(l))
+# # [10,20,30] => list
+# # (10,20,30) => tuple
+# # {10,20,30} => set
+# #{100:"durga"} => dict
+# l =[10,"durga",20,10,30]
+# print(l[0]) #=> 10
+# print(l[-1]) #=> 30
+# print(l[1:4])#=> ['durga', 20, 10]   ,list of the element from end (4-1/3)
+
+
+s = []
+s.append(10)#append = add
+print(s)
+s.append(30)
+print(s)
+s.append(40)
+print(s)
+s.remove(30)
+print(s)
