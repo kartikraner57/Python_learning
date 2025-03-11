6ede2505c14b842961cf6e3029e9492124649881
kartikraner57 <kartikraner06@gmail.com>
2025-03-11 22:42:57 +0530

    add

---
+++ 5)Input output statement/raw-input.py
@@ -0,0 +1,10 @@
+#raw input is not applicable in python 3x or python 3.o
+#raw_input is equal to input function in python 3.o
+#this provide only string value
+
+
+#in python 3.o   input()
+x= input('input same thing :')
+print(type(x)) #=>3
+y = input('inter same value :')
+print(type(x))
