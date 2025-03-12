12eb1f577913e67e2945acb27462941951a0fd24
kartikraner57 <kartikraner06@gmail.com>
2025-03-12 08:27:13 +0530

    add

--- 5)Input output statement/raw-input.py
+++ 5)Input output statement/raw-input.py
@@ -1,18 +1,23 @@
-# 33
-
-
+# 68
+#raw_input satment => is use only in python 2.o,it's porvide only string value,
+#input satment in python 2.o => its's provide int,float,string value
+#input stament in python 3.o => it's porvide only string value,it is similar as raw_input statment .
+x = input('enter same thing :')
+print(type(x)) #=> str
 
 #______________________
-# #69 lecture
+# # #69 lecture
 
 
-# x =input("inter first number :")
-# y =input("inter second number :")
-# i =int(x)
-# j =int(y)
-# print("the sum :",j + i)
+# # x =input("inter first number :")
+# # y =input("inter second number :")
+# # i =int(x)
+# # j =int(y)
+# # print("the sum :",j + i)
 
 
-x = int(input("enter first number :"))
-y = int(input("enter second number :"))
-print("the sum :",x+y)
+# x = int(input("enter first number :"))
+# y = int(input("enter second number :"))
+# print("the sum :",x+y)
+
+print("thesum : ",int(input("enter first number :"))+int(input("enter the 2nd number :")))
