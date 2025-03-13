ae663366afff160a5eb650e009e45984a67a3bc3
kartikraner57 <kartikraner06@gmail.com>
2025-03-13 23:23:20 +0530

    add

--- 5)Input output statement/raw-input.py
+++ 5)Input output statement/raw-input.py
@@ -2,8 +2,8 @@
 #raw_input satment => is use only in python 2.o,it's porvide only string value,
 #input satment in python 2.o => its's provide int,float,string value
 #input stament in python 3.o => it's porvide only string value,it is similar as raw_input statment .
-x = input('enter same thing :')
-print(type(x)) #=> str
+# x = input('enter same thing :')
+# print(type(x)) #=> str
 
 #______________________
 # # #69 lecture
@@ -20,4 +20,24 @@ print(type(x)) #=> str
 # y = int(input("enter second number :"))
 # print("the sum :",x+y)
 
-print("thesum : ",int(input("enter first number :"))+int(input("enter the 2nd number :")))
+# print("thesum : ",int(input("enter first number :"))+int(input("enter the 2nd number :")))
+
+
+###___________________________________________________________________________________________________________
+##3lecture 69
+###write a program to need employe data from the keyboard and print that data
+eno = int(input('enter employe number :'))
+ename =input('enter a employe name:')
+esal= float(input('enter a employe salery :'))
+eaddar = input('enter a employe adress :')
+marrid = bool(input('employ marrrid ?[True,False]:'))
+
+print("pleasse conform your provided information")
+print("employe number :",eno)
+print("employe name :",ename)
+print("employe salery :",esal)
+print("employe adress :",eaddar)
+print("employe married :",marrid)
+print("employe number :",marrid)
+print("employe number : eno")
+
