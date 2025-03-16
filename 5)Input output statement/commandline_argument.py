abf83c6201c98f9164f6c7a4588ecad254b638b7
kartikraner57 <kartikraner06@gmail.com>
2025-03-16 15:47:38 +0530

    add

--- 5)Input output statement/commandline_argument.py
+++ 5)Input output statement/commandline_argument.py
@@ -6,12 +6,18 @@
 # print(argv[0])       # Prints the script name (first element in argv)
 # print(type(argv))    # Prints the type of argv (which is a list)
 
+# from sys import argv
+
+# args = argv[1:]  # Exclude script name
+# total = 0  # Avoid using 'sum' as it's a built-in function
+
+# for x in args:
+#     total += int(x)  # Convert to integer and add
+
+# print("Sum:", total)
+
 from sys import argv
-
-args = argv[1:]  # Exclude script name
-total = 0  # Avoid using 'sum' as it's a built-in function
-
-for x in args:
-    total += int(x)  # Convert to integer and add
-
-print("Sum:", total)
+print(argv[0])
+# print(argv[1])
+# print(argv[2])
+print (argv)
---
+++ 5)Input output statement/outpu_tstatement.py
@@ -0,0 +1,49 @@
+# output statment
+# #######print()
+
+
+####without arrguments :
+
+print("durga")
+print()
+print('soft')
+
+
+
+
+####witharguments
+# print(string)
+print('durga')
+print("duga \n software")
+print("durga \t software")
+print("duga" + "software")
+print(10* "software")
+
+# #########pri   stament with variable number of arguments
+a, b, c = 10, 20, 30
+print("value", a, b, c)
+print("Sum:", a + b + c)  # Correct way to sum the values
+
+
+
+################ sep atributes       sep means  sepreator
+a, b, c, d =  10, 20  , 30, 40,
+print(a,b,c,d ,sep = ':')
+print(a,b,c,d ,sep = '.')
+print(a,b,c,d ,sep = '__')
+
+
+
+
+################ end atributes
+print("hello")
+print('durga')
+print('soft')
+print("hello",end = '')
+print('durga',end ='')
+print('soft')
+
+
+print("hello",end = ': :')
+print('durga',end ='****')
+print('soft')
