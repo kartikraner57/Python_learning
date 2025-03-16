fc26cd28e37961aa3ca55d477644a86efe55fa8a
kartikraner57 <kartikraner06@gmail.com>
2025-03-16 15:21:46 +0530

    add

---
+++ 5)Input output statement/commandline_argument.py
@@ -0,0 +1,17 @@
+#######3command line argumments
+#### |py |test.py| 10 | 20 | 30| # 0 1 2 3
+# from sys import argv
+
+# print(argv)          # Prints the list of command-line arguments
+# print(argv[0])       # Prints the script name (first element in argv)
+# print(type(argv))    # Prints the type of argv (which is a list)
+
+from sys import argv
+
+args = argv[1:]  # Exclude script name
+total = 0  # Avoid using 'sum' as it's a built-in function
+
+for x in args:
+    total += int(x)  # Convert to integer and add
+
+print("Sum:", total)
