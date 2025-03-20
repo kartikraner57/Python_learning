3e40ea755b23b49c181811e31babc3d7f86bc376
kartikraner57 <kartikraner06@gmail.com>
2025-03-20 09:39:36 +0530

    add

---
+++ 6)Python flow control part 1/tansfer_statment.py
@@ -0,0 +1,38 @@
+############ Transfer Statements : 1)break statment
+########## 1) break: We can use break statement inside loops to break loop execution based on some condition. 
+
+# for i in range(10): #i = 10
+#     if i==7: 
+#         print("processing is enough,break loop exicution") 
+#         break 
+#     print(i)
+
+# for i in range(10): #i = 10
+#     if i==8: 
+#         print("processing is enough,break loop exicution") 
+#         break 
+#     print(i)
+# print('outside of loop')
+
+
+# cart =[10,20,600,70,80]
+# for item in cart :
+#     if item >600:
+#         print("to place this order insurence must be required")
+#         break 
+#     print("processing item ",item)
+
+# cart=[10,20,600,60,70] 
+# for item in cart: 
+#     if item >100: 
+#         print("To place this order insurence must be required") 
+#         break 
+#     print(item)
+
+
+
+x =10 
+if x > 40:
+    print("wea are stoping progam")
+    break 
+print("helow")
