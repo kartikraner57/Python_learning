da1e7a5ca3a07625a6495562eaa4513e7fcdc494
kartikraner57 <kartikraner06@gmail.com>
2025-03-22 23:49:59 +0530

    add

---
+++ 6)Python flow control part 1/pass_stetment.py
@@ -0,0 +1,31 @@
+########## 3) pass statement:  pass is a keyword in Python. 
+#  In our programming syntactically if block is required which won't do anything then we can define that empty block with pass keyword.
+##########pass=> |- It is an empty statement |- It is null statement |- It won't do anything
+######## pass satment acts as empty stament acts as aplace for future implimentation
+# class A:
+#     pass
+# class B:
+#     pass
+# class C:
+#     pass
+
+# x = 100
+# if x >10:
+#     print("success")
+
+# for i in range(100): 
+#     if i%9==0: 
+#         print(i)
+#     else:pass
+x = int(input("enter marks :"))
+if x >= 35:
+    print("success")
+else :
+    pass
+
+
+x = int(input("enter marks :"))
+if x >= 35:
+    print("success")
+else :
+    print("fail")
