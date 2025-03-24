b9cc4162b57535e2235be0077321ee8201876599
kartikraner57 <kartikraner06@gmail.com>
2025-03-24 23:13:33 +0530

    add

--- 5)Input output statement/outpu_tstatement.py
+++ 5)Input output statement/outpu_tstatement.py
@@ -1,67 +1,67 @@
-# output statment
-# #######print()
+# # output statment
+# # #######print()
 
 
-####without arrguments :
+# ####without arrguments :
 
-print("durga")
-print()
-print('soft')
-
-
-
-
-#### witharguments
-# print(string)
-print('durga')
-print("duga \n software")
-print("durga \t software")
-print("duga" + "software")
-print(10* "software")
-
-# #########print   stament with variable number of arguments
-a, b, c = 10, 20, 30
-print("value", a, b, c)
-print("Sum:", a + b + c)  # Correct way to sum the values
-
-
-
-################ sep atributes       sep means  sepreator
-a, b, c, d =  10, 20  , 30, 40,
-print(a,b,c,d ,sep = ':')
-print(a,b,c,d ,sep = '.')
-print(a,b,c,d ,sep = '__')
+# print("durga")
+# print()
+# print('soft')
 
 
 
 
-################ end atributes /end Parameter 
-print("hello")
-print('durga')
-print('soft')
-print("hello",end = '')
-print('durga',end ='')
-print('soft')
+# #### witharguments
+# # print(string)
+# print('durga')
+# print("duga \n software")
+# print("durga \t software")
+# print("duga" + "software")
+# print(10* "software")
+
+# # #########print   stament with variable number of arguments
+# a, b, c = 10, 20, 30
+# print("value", a, b, c)
+# print("Sum:", a + b + c)  # Correct way to sum the values
 
 
-print("hello",end = ': :')
-print('durga',end ='****')
-print('soft')
+
+# ################ sep atributes       sep means  sepreator
+# a, b, c, d =  10, 20  , 30, 40,
+# print(a,b,c,d ,sep = ':')
+# print(a,b,c,d ,sep = '.')
+# print(a,b,c,d ,sep = '__')
 
 
-print(10,20,30,40,sep =':',end = '***')
-
-print(40,50,60,sep = ':')
-print(70,80,sep ='**',end = '$$$')
-print(90,100)
 
 
-### print(object)
-l =[10,20,30.40]
-print(l)
-t = (10,20,30,40)
-print(t)
-################ print() with replacemnet operator {}
+# ################ end atributes /end Parameter 
+# print("hello")
+# print('durga')
+# print('soft')
+# print("hello",end = '')
+# print('durga',end ='')
+# print('soft')
+
+
+# print("hello",end = ': :')
+# print('durga',end ='****')
+# print('soft')
+
+
+# print(10,20,30,40,sep =':',end = '***')
+
+# print(40,50,60,sep = ':')
+# print(70,80,sep ='**',end = '$$$')
+# print(90,100)
+
+
+# ### print(object)
+# l =[10,20,30.40]
+# print(l)
+# t = (10,20,30,40)
+# print(t)
+# ################ print() with replacemnet operator {}
 name = "durga"
 salory = 10000
 gf =' sunny'
@@ -70,21 +70,21 @@ print("hello {0}, your salory is {1} and you find {2} waiting".format(name,salor
 print("hello {n}, your salory is {s} and you find {f} waiting".format(s =name,n =salory,f =gf))
 
 
-a , b ,c,d = 10 ,20 ,30, 40
-print('a = {}, b={}, c={}, d={}'.format(a,b,c,d))
+# a , b ,c,d = 10 ,20 ,30, 40
+# print('a = {}, b={}, c={}, d={}'.format(a,b,c,d))
 
 
-################ print stament with formated string
-##### syntax = print("formated string" % (veriable list))
-a = 10 
-print("a value : %i" %a)
+# ################ print stament with formated string
+# ##### syntax = print("formated string" % (veriable list))
+# a = 10 
+# print("a value : %i" %a)
 
 
-a = 10 
-b = 0
-c = 30
-print("a = %d, b = %d, c = %d"%(a,b,c))
+# a = 10 
+# b = 0
+# c = 30
+# print("a = %d, b = %d, c = %d"%(a,b,c))
 
-name = "durga"
-marks = [10,20,30,40]
-print('hello %s,your marks list : %s' %(name, marks))
+# name = "durga"
+# marks = [10,20,30,40]
+# print('hello %s,your marks list : %s' %(name, marks))
---
+++ python string/string.py
@@ -0,0 +1,37 @@
+# ch = 'a'
+# type(ch)
+# print(ch)
+
+
+# s = """"durga
+# software
+# solution"""
+# print(s)
+
+# #s = 'This is ' single quote symbol' =>Invalid
+# s = 'This is \' single quote symbol' #=> Valid
+# s = "This is ' single quote symbol" #=> Valid
+# s = 'This is " double quotes symbol' #=> Valid
+## s = 'The "Python Notes" by 'durga' is very helpful'  => Invalid
+## s = "The "Python Notes" by 'durga' is very helpful" => Invalid
+## s = 'The \"Python Notes\" by \'durga\' is very helpful'=>Valid
+## s = '''The "Python Notes" by 'durga' is very helpful'''=> Valid
+
+# print(s)
+
+
+# f = '''python "notes" by 'durga' is very good'''
+# print(f)
+# c = "python notes by 'durga' is very good"
+# print(f)
+# l = 'python notes by\' "durga" is very good'
+# print(l)
+
+############wap to display of given index(both +ve and -ve) of string
+s = input("enter same string :")
+# i = 0
+# for x in s:
+#     print("the character present at +ve index : {} and at -ve index: {} is : {} ".format(i, i-len(s),x))
+#     i = i+1
+
+
