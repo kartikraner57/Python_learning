29af5a5961bf703fe1d771135a65abac297bfc6c
kartikraner57 <kartikraner06@gmail.com>
2025-03-26 09:20:34 +0530

    dd

---
+++ python string/operator_for_string.py
@@ -0,0 +1,13 @@
+# k
+#########compresion operator 
+
+s1 = input("enetr first string :")
+s2 = input("enetr second string :")
+if s1 == s2:
+    print("both string are equal")
+elif s1 < s2 :
+    print("first is less than second string")
+else :
+    print("first string is greater than second string")
+
+
