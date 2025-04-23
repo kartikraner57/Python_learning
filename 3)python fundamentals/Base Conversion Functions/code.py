1dfc687640de8e103da4ff984756065de67eb3cc
kartikraner57 <kartikraner06@gmail.com>
2025-04-23 23:28:12 +0530

    Add

--- 3)python fundamentals/Base Conversion Functions/code.py
+++ 3)python fundamentals/Base Conversion Functions/code.py
@@ -8,3 +8,15 @@ print(oct(0b111101))
 print(oct(0xFace))
 print(hex(1000))
 print(hex(0o123456))
+result = z1 + z2
+print(result)  # Output: (4+6j)
+
+result = z1 - z2
+print(result)  # Output: (2+2j)
+
+result = z1 * z2
+print(result)  # Output: (-5+10j)
+
+result = z1 / z2
+print(result)  # Output: (2.2+0.4j)
+  
