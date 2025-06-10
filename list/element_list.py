5b822929d2c63f0aaf85082863e2344949511b70
kartikraner57 <kartikraner06@gmail.com>
2025-06-10 17:40:53 +0530

    add

--- list/element_list.py
+++ list/element_list.py
@@ -32,3 +32,7 @@
 
 # Note: If the specified index is greater than max index then element will be inserted at last position.
 #  If the specified index is smaller than min index then element will be inserted at first position.ldl sssssss
+
+
+# Note: If the specified index is greater than max index then element will be inserted at last position.
+#  If the specified index is smaller than min index then element will be inserted at first position.ldl sssssss
