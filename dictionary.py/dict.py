5db7d3a1004df7d4e8b02bc9972adadf8870fb65
Kartik <kartikraner06gmail.com>
2025-06-11 16:45:00 +0530

    add

---
+++ dictionary.py/dict.py
@@ -0,0 +1,11 @@
+d = {}                 
+d[100] = "durga"       
+d[200] = "ravi"        
+d[300] = "shiva"      
+
+print(d)               
+
+
+d = {100: 'durga', 200: 'ravi', 300: 'shiva'}  
+print(d[100])  
+print(d[300])  
