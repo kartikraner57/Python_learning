d1f64fccd5094b2c317ee8365f1b46dd52b509c6
Kartik <kartikraner06gmail.com>
2025-06-11 17:56:09 +0530

    add

--- hello.py
+++ hello.py
@@ -93,3 +93,8 @@ GeneratorExit
 # print(str.find("a"))
 # print(str.find("from"))
 GeneratorExit 
+ print(str.replace("o","a"))
+# print(str.find("o"))
+# print(str.find("a"))
+# print(str.find("from"))
+GeneratorExit gi
