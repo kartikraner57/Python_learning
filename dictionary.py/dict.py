9e74b5d7b25f684f6c169e1889735865dde1651a
Kartik <kartikraner06gmail.com>
2025-06-11 16:54:00 +0530

    add

--- dictionary.py/dict.py
+++ dictionary.py/dict.py
@@ -2,10 +2,25 @@ d = {}
 d[100] = "durga"       
 d[200] = "ravi"        
 d[300] = "shiva"      
-
 print(d)               
 
-
+####   writing a Python program that stores student names and their marks using a dictionary, and then prints the data
 d = {100: 'durga', 200: 'ravi', 300: 'shiva'}  
 print(d[100])  
 print(d[300])  
+
+rec = {}  # Step 1: Create an empty dictionary
+
+n = int(input("Enter number of students: "))  # Step 2: Input total number of students
+
+i = 1
+while i <= n:  # Step 3: Loop for 'n' students
+    name = input("Enter Student Name: ")  # Step 4: Input student name
+    marks = input("Enter % of Marks of Student: ")  # Step 5: Input student marks
+    rec[name] = marks  # Step 6: Add name:marks pair to dictionary
+    i += 1  # Step 7: Increment loop counter
+
+# Step 8: Print the results
+print("\nName of Student", "\t", "% of Marks")
+for x in rec:
+    print("\t", x, "\t\t", rec[x])
