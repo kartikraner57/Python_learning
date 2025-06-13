0c013db6c5c69853d883c17ad16cd967bfc1820b
Kartik <kartikraner06@gmail.com>
2025-06-13 23:04:47 +0530

    add

---
+++ dictionary.py/q_ans.py
@@ -0,0 +1,8 @@
+################### Q)wap to get value the dictionary for the given key
+# d = {100: "A", 200: "B", 300: "C", 400: "D"}
+# key = int(input("Enter the key to find value: "))
+
+# if key in d:
+#     print("The corresponding value:", d.get(key))
+# else:
+#     print("Key not found in dictionary.")
