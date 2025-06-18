c8bd055347972cb4f8b299b77e23b8c64e143e06
Kartik <kartikraner06@gmail.com>
2025-06-18 23:48:03 +0530

    add

--- functions/user defined function.py
+++ functions/user defined function.py
@@ -3,3 +3,10 @@ def sub(a, b):
 
 sub(100, 200)
 sub(200, 100)
+
+
+def wish(name, msg):
+    print("Hello", name, msg)
+
+wish(name="Durga", msg="Good Morning")
+wish(msg="Good Morning", name="Durga")
