9fce3f1c0b46223421a1776ae6f22ba1507beb66
Kartik <kartikraner06@gmail.com>
2025-06-19 22:57:31 +0530

    addd

--- functions/parameters.py
+++ functions/parameters.py
@@ -1,5 +1,14 @@
-def wish(name): 
-    print("Hello", name, "Good Morning")
+# # Eg: Write a function to take name of the student as input and print wish message by name.
+# def wish(name): 
+#     print("Hello", name, "Good Morning")
 
-wish("Durga")
-wish("Ravi")
+# wish("Durga")
+# wish("Ravi")
+
+
+# Eg: Write a function to take number as input and print its square value
+def squareIt(number):
+    print("The Square of", number, "is", number * number)
+
+squareIt(4)
+squareIt(5)
