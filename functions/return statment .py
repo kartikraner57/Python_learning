a30b7a59c6cf63b090b3a81e492d56eb2a905e29
Kartik <kartikraner06@gmail.com>
2025-06-19 23:43:47 +0530

    add

---
+++ functions/return statment .py
@@ -0,0 +1,7 @@
+# Q) Write a Function to accept 2 Numbers as Input and return Sum
+def add(x, y):
+    return x + y
+
+result = add(10, 20)
+print("The sum is", result)
+print("The sum is", add(100, 200))
