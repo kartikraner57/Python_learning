567ad39a5eaf7f279691704c9b2cf0b2f9af7bff
Kartik <kartikraner06@gmail.com>
2025-06-20 23:36:25 +0530

    add

--- functions/return statment .py
+++ functions/return statment .py
@@ -5,3 +5,19 @@ def add(x, y):
 result = add(10, 20)
 print("The sum is", result)
 print("The sum is", add(100, 200))
+# If we are not writing return statement then default return value is None.
+def f1():
+    print("Hello")
+
+f1()
+print(f1())
+# Write a Function to check whether the given Number is Even OR Odd? 
+def even_odd(num):
+    if num % 2 == 0:
+        print(num, "is Even Number")
+    else:
+        print(num, "is Odd Number")
+
+even_odd(10)
+even_odd(15)
+# Q) Write a Function to find Factorial of given Number?
