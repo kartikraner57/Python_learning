01bab7053cd77a27c20851297cf820e836d85b03
Kartik <kartikraner06@gmail.com>
2025-06-21 23:50:48 +0530

    add~c?

--- functions/typesofvariables.py
+++ functions/typesofvariables.py
@@ -10,13 +10,28 @@ def f2():
 f1()
 f2()
 
-# 
+# 2) Local Variables:
 def f1():
     a = 10
     print(a)  # valid
 
 def f2():
     print(a)  # invalid
+
+f1()
+f2()
+
+# 3)global Keyword:
+
+
+a = 10  # global variable
+
+def f1():
+    a = 777  # local variable
+    print(a)  # prints local 'a'
+
+def f2():
+    print(a)  # prints global 'a'
 
 f1()
 f2()
