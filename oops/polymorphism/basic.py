049c5c27953c7c102bd744fab6dc7950c8e686df
Kartik <kartikraner06@gmail.com>
2025-09-01 01:13:09 +0530

    Ad

--- oops/polymorphism/basic.py
+++ oops/polymorphism/basic.py
@@ -37,3 +37,24 @@ print("s1>s2 =", s1 > s2)
 print("s1<s2 =", s1 < s2)
 print("s1<=s2 =", s1 <= s2)
 print("s1>=s2 =", s1 >= s2)
+
+
+
+class Employee:
+    def __init__(self, name, salary):
+        self.name = name
+        self.salary = salary
+
+    def __mul__(self, other):
+        return self.salary * other.days
+
+
+class TimeSheet:
+    def __init__(self, name, days):
+        self.name = name
+        self.days = days
+
+
+e = Employee('Durga', 500)
+t = TimeSheet('Durga', 25)
+print('This Month Salary:', e * t)
