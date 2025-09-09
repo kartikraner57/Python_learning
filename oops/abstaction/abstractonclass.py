dacf87b1cfe47cb9867a45ab58307f89def7c274
Kartik <kartikraner06@gmail.com>
2025-09-09 23:59:21 +0530

    ADd

--- oops/abstaction/abstractonclass.py
+++ oops/abstaction/abstractonclass.py
@@ -3,4 +3,4 @@ from abc import *   # Import everything from abc module (Abstract Base Classes)
 class Test:         # Define a normal class 'Test'
     pass            # 'pass' means "do nothing", an empty class
 
-t = Test()          # Create an object of Test
+t = Test()          # Create an object of Tes
