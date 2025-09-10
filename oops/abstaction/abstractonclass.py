6f5e6ef85b845470e4b792acafb1c601cad68149
Kartik <kartikraner06@gmail.com>
2025-09-10 23:59:59 +0530

    A

--- oops/abstaction/abstractonclass.py
+++ oops/abstaction/abstractonclass.py
@@ -4,3 +4,11 @@ class Test:         # Define a normal class 'Test'
     pass            # 'pass' means "do nothing", an empty class
 
 t = Test()          # Create an object of Tes
+try:
+    x = int(input("Enter First Number: "))
+    y = int(input("Enter Second Number: "))
+    print(x / y)
+except ZeroDivisionError:
+    print("ZeroDivisionError")
+except ArithmeticError:
+    print("ArithmeticError")
