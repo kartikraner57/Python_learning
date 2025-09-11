761f6101a9dcf70eddc285d5adcc6cd8b475077f
Kartik <kartikraner06@gmail.com>
2025-09-11 12:04:35 +0530

    ad

--- exception/a.py
+++ exception/a.py
@@ -17,3 +17,19 @@ except ZeroDivisionError:
     print(10/2)
 
 print("stmt-3")
+# How to Print Exception Information:
+try:
+    print(10/0)
+except ZeroDivisionError as msg:
+    print("exception raised and its description is:", msg)
+# try with Multiple except Blocks:
+try:
+    x = int(input("Enter First Number: "))
+    y = int(input("Enter Second Number: "))
+    print(x / y)
+
+except ZeroDivisionError:
+    print("Can't Divide with Zero")
+
+except ValueError:
+    print("please provide int value only")
