5a0847ad0a3079e2c812163c15c773de0f7db950
Kartik <kartikraner06@gmail.com>
2025-09-14 07:56:44 +0530

    AD

--- file handling/fily.py
+++ file handling/fily.py
@@ -6,3 +6,11 @@ print("Is File Writable: ",f.writable())
 print("Is File Closed : ",f.closed)
 f.close()
 print("Is File Closed : ",f.closed) 
+
+
+f=open("abcd.txt",'w')
+f.write("Durga\n")
+f.write("Software\n")
+f.write("Solutions\n")
+print("Data written to the file successfully")
+f.close() 
