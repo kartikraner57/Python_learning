7f0b4b5cdfef8ed851499a1934c66544327201f1
Kartik <kartikraner06@gmail.com>
2025-09-28 15:19:39 +0530

    add

--- file handling/fily.py
+++ file handling/fily.py
@@ -1,3 +1,5 @@
+# Various Properties of File Object:
+
 # f=open("abc.txt",'w')
 # print("File Name: ",f.name)
 # print("File Mode: ",f.mode)
@@ -15,9 +17,38 @@
 # print("Data written to the file successfully")
 # f.close() 
 
-f=open("abcd.txt",'a')
-f.write("Durga\n")
-f.write("Software\n")
-f.write("Solutions\n")
-print("Data written to the file successfully")
-f.close() 
+# f=open("abcd.txt",'a')
+# f.write("Durga\n")
+# f.write("Software\n")
+# f.write("Solutions\n")
+# print("Data written to the file successfully")
+# f.close() 
+
+# # Writing Data to Text Files
+
+# f = open("abcd.txt", "w")
+# f.write("Durga\n")
+# f.write("Software\n")
+# f.write("Solutions\n")
+# print("Data written to the file successfully")
+# f.close()
+
+
+
+# f = open("abcd.txt", "w")
+# list = ["sunny\n", "bunny\n", "vinny\n", "chinny"]
+# f.writelines(list)
+# print("List of lines written to the file successfully")
+# f.close()
+
+
+# # Reading Character Data from Text Files:
+# f = open("abc.txt", "r")
+# data = f.read()
+# print(data)
+# f.close()
+
+# Read entire file
+f
+f = open("abc.txt", "r")
+data = f
