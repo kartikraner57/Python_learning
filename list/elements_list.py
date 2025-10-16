628cd74c61eb2002705e4659926b262afe7b34f2
Kartik <kartikraner06@gmail.com>
2025-10-16 23:22:52 +0530

    a

--- list/elements_list.py
+++ list/elements_list.py
@@ -11,6 +11,13 @@
 # ֍ print(list[10])  IndexError: list index out of range
 
 
+#2) By using Slice Operator:  
+# 
+# Syntax: list2 = list1[start:stop:step]
+# Start  It indicates the Index where slice has to Start Default Value is 0
+# Stop  It indicates the Index where slice has to End Default Value is max allowed Index of List ie Length of the List
+# Step  increment value Default Value is 1
+
 
 # n=[1,2,3,4,5,6,7,8,9,10] 
 # #2) print(n[2:7:2]) 
