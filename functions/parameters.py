913be0b15d6d4d407c67e55ec848081d7c303208
Kartik <kartikraner06@gmail.com>
2025-11-11 23:26:04 +0530

    a

--- functions/parameters.py
+++ functions/parameters.py
@@ -6,4 +6,9 @@
 # wish("Ravi")
 
 
+# # Eg: Write a function to take number as input and print its square value
+# def squareIt(number):
+#     print("The Square of", number, "is", number * number)
 
+# squareIt(6)
+# squareIt(5)
