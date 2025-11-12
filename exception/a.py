e2ddb49ae38e00e758ba239d8ec8ec913ab5108f
Kartik <kartikraner06@gmail.com>
2025-11-12 23:51:45 +0530

    ad

--- exception/a.py
+++ exception/a.py
@@ -99,15 +99,4 @@
 #     print("Default Except: Plz provide valid input only")
 
 
-# x = int(input("Enter First Number: "))
-# y = int(input("Enter Second Number: "))
-# print(x / y)
-
-# try:
-#     print(10/0)
-# except:
-#     print("Default Except")
-# except ZeroDivisionError:
-#     print("ZeroDivisionError"
-
 
