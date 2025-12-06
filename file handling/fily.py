878904055c6dc6e1e376a39b59ec7ce0883311a6
Kartik <kartikraner06@gmail.com>
2025-12-06 23:04:13 +0530

    a

--- file handling/fily.py
+++ file handling/fily.py
@@ -35,10 +35,5 @@
 
 
 
-# f = open("abcd.txt", "w")
-# list = ["sunny\n", "bunny\n", "vinny\n", "chinny"]
-# f.writelines(list)
-# print("List of lines written to the file successfully")
-# f.close()
 
 
